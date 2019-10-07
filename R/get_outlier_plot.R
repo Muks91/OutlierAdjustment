ea575a37c255bb570eb61cadb9817185a14d8a24
Mukesh Sharma <mukesh.sharma@globalservs.com>
2019-10-07 15:47:27 +0100

    Create package

---
+++ R/get_outlier_plot.R
@@ -0,0 +1,23 @@
+#' Identifies and adjust outliers in data
+#'
+#' This function identifies and plots outliers
+#' @param dataframe Object of data table or dataframe
+#' @param column Column in questions with double quotes
+#' @export get_outlier_plot
+#' @examples
+#' Example <- get_outlier_plot()
+get_outlier_plot <- function(dataframe,column){
+
+  geombox<- dataframe %>%
+    ggplot2::ggplot() +
+    ggplot2::geom_boxplot(mapping = aes_string(y=column)) +
+    ggtitle(paste0("Boxplot of", " ",column)) +
+    ylab(column)
+  p<- plotly_build(geombox)
+
+  for(i in 1:length(p$x$data))
+    p$x$data[[i]]$marker$size = 10
+  p$x$data[[i]]$marker$line$color = "red"
+
+  p
+}
